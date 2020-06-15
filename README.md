e14804be4a85d034e8d1a04b3029f61d8b09b8c3
Wolfgang Lehrach <wolf@vicarious.com>
2020-06-15 14:14:24 -0700

    Allow pip installable clang-format

---
+++ README.md
@@ -0,0 +1,4 @@
+# pre-commit-pip-clang-format
+
+Use pip installable clang-format and allow use as pre-commit hook.
+
---
+++ setup.py
@@ -0,0 +1,7 @@
+import setuptools
+
+setuptools.setup(
+    install_requires=['clang-format==9.0.0'],
+    name='dummy-pre-commit-clang-format',
+    version='9.0.0',
+)
